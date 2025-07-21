39129624d87919a0734d8e344346c2cc387d9790
Sascha Rose <sascha.rose@gmail.com>
2025-07-21 14:35:05 +0200

    refactoring

--- projects/api/src/dataAccessLayer.ts
+++ projects/api/src/dataAccessLayer.ts
@@ -1,12 +1,14 @@
+import { dirname, join } from "node:path";
 import { DatabaseSync } from "node:sqlite";
-import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { TeacherRepository } from "./repositories/TeacherRepository.js";
-import { CourseRepository } from "./repositories/CourseRepository.js";
-import { StudentRepository } from "./repositories/StudentRepository.js";
-import { LessonRepository } from "./repositories/LessonRepository.js";
-import { CourseStudentRepository } from "./repositories/CourseStudentRepository.js";
-import { LessonStudentRepository } from "./repositories/LessonStudentRepository.js";
+import {
+  CourseRepository,
+  CourseStudentRepository,
+  LessonRepository,
+  LessonStudentRepository,
+  StudentRepository,
+  TeacherRepository,
+} from "./repositories";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -31,16 +33,6 @@ export class DataAccessLayer {
     this.lessons = new LessonRepository(this.db);
     this.courseStudents = new CourseStudentRepository(this.db);
     this.lessonStudents = new LessonStudentRepository(this.db);
-  }
-
-  async withTransaction<T>(callback: () => T): Promise<T> {
-    if (!this.db) {
-      throw new Error("Database not initialized");
-    }
-
-    // Note: For the in-memory implementation, we don't have real transactions
-    // but we can add this functionality later when needed
-    return callback();
   }
 
   close() {
---
+++ projects/api/src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from "./CourseRepository";
+export * from "./StudentRepository";
+export * from "./TeacherRepository";
+export * from "./LessonRepository";
+export * from "./CourseStudentRepository";
+export * from "./LessonStudentRepository";
