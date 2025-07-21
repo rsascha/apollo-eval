ed3c99ba73fe6f7cc266fd039dae9fc3d68dadc8
Sascha Rose <sascha.rose@gmail.com>
2025-07-21 15:40:43 +0200

    backup

--- projects/api/codegen.ts
+++ projects/api/codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://localhost:4000/graphql",
-  documents: "**/*.graphql",
+  documents: "../web-ui/src/queries/**/*.ts",
   generates: {
     "./src/types.ts": { plugins: ["typescript", "typescript-operations"] },
   },
---
+++ projects/api/src/types.ts
@@ -0,0 +1,420 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type CompleteLessonInput = {
+  course_id: Scalars['Int']['input'];
+  finished: Scalars['String']['input'];
+  sequence_number: Scalars['Int']['input'];
+  student_id: Scalars['Int']['input'];
+};
+
+export type Course = {
+  __typename?: 'Course';
+  description?: Maybe<Scalars['String']['output']>;
+  duration_hours?: Maybe<Scalars['Int']['output']>;
+  id: Scalars['Int']['output'];
+  lessons: Array<Lesson>;
+  students: Array<Student>;
+  teacher: Teacher;
+  teacher_id: Scalars['Int']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type CourseEnrollment = {
+  __typename?: 'CourseEnrollment';
+  course: Course;
+  course_id: Scalars['Int']['output'];
+  student: Student;
+  student_id: Scalars['Int']['output'];
+};
+
+export type CreateCourseInput = {
+  description?: InputMaybe<Scalars['String']['input']>;
+  duration_hours?: InputMaybe<Scalars['Int']['input']>;
+  teacher_id: Scalars['Int']['input'];
+  title: Scalars['String']['input'];
+};
+
+export type CreateLessonInput = {
+  content?: InputMaybe<Scalars['String']['input']>;
+  course_id: Scalars['Int']['input'];
+  sequence_number: Scalars['Int']['input'];
+  title: Scalars['String']['input'];
+};
+
+export type CreateStudentInput = {
+  email?: InputMaybe<Scalars['String']['input']>;
+  name: Scalars['String']['input'];
+};
+
+export type CreateTeacherInput = {
+  area?: InputMaybe<Scalars['String']['input']>;
+  name: Scalars['String']['input'];
+};
+
+export type EnrollStudentInput = {
+  course_id: Scalars['Int']['input'];
+  student_id: Scalars['Int']['input'];
+};
+
+export type Lesson = {
+  __typename?: 'Lesson';
+  content?: Maybe<Scalars['String']['output']>;
+  course: Course;
+  course_id: Scalars['Int']['output'];
+  sequence_number: Scalars['Int']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type LessonCompletion = {
+  __typename?: 'LessonCompletion';
+  course_id: Scalars['Int']['output'];
+  finished: Scalars['String']['output'];
+  lesson: Lesson;
+  sequence_number: Scalars['Int']['output'];
+  student: Student;
+  student_id: Scalars['Int']['output'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  completeLesson: LessonCompletion;
+  createCourse: Course;
+  createLesson: Lesson;
+  createStudent: Student;
+  createTeacher: Teacher;
+  deleteCourse: Scalars['Boolean']['output'];
+  deleteLesson: Scalars['Boolean']['output'];
+  deleteStudent: Scalars['Boolean']['output'];
+  deleteTeacher: Scalars['Boolean']['output'];
+  enrollStudent: CourseEnrollment;
+  uncompleteLesson: Scalars['Boolean']['output'];
+  unenrollStudent: Scalars['Boolean']['output'];
+  updateCourse?: Maybe<Course>;
+  updateLesson?: Maybe<Lesson>;
+  updateStudent?: Maybe<Student>;
+  updateTeacher?: Maybe<Teacher>;
+};
+
+
+export type MutationCompleteLessonArgs = {
+  input: CompleteLessonInput;
+};
+
+
+export type MutationCreateCourseArgs = {
+  input: CreateCourseInput;
+};
+
+
+export type MutationCreateLessonArgs = {
+  input: CreateLessonInput;
+};
+
+
+export type MutationCreateStudentArgs = {
+  input: CreateStudentInput;
+};
+
+
+export type MutationCreateTeacherArgs = {
+  input: CreateTeacherInput;
+};
+
+
+export type MutationDeleteCourseArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type MutationDeleteLessonArgs = {
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+};
+
+
+export type MutationDeleteStudentArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type MutationDeleteTeacherArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type MutationEnrollStudentArgs = {
+  input: EnrollStudentInput;
+};
+
+
+export type MutationUncompleteLessonArgs = {
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+};
+
+
+export type MutationUnenrollStudentArgs = {
+  courseId: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+};
+
+
+export type MutationUpdateCourseArgs = {
+  id: Scalars['Int']['input'];
+  input: CreateCourseInput;
+};
+
+
+export type MutationUpdateLessonArgs = {
+  content?: InputMaybe<Scalars['String']['input']>;
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+  title?: InputMaybe<Scalars['String']['input']>;
+};
+
+
+export type MutationUpdateStudentArgs = {
+  id: Scalars['Int']['input'];
+  input: CreateStudentInput;
+};
+
+
+export type MutationUpdateTeacherArgs = {
+  id: Scalars['Int']['input'];
+  input: CreateTeacherInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  completions: Array<LessonCompletion>;
+  course?: Maybe<Course>;
+  courses: Array<Course>;
+  enrollments: Array<CourseEnrollment>;
+  lessons: Array<Lesson>;
+  lessonsByCourse: Array<Lesson>;
+  student?: Maybe<Student>;
+  studentProgress: Array<Lesson>;
+  students: Array<Student>;
+  teacher?: Maybe<Teacher>;
+  teachers: Array<Teacher>;
+};
+
+
+export type QueryCourseArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type QueryLessonsByCourseArgs = {
+  courseId: Scalars['Int']['input'];
+};
+
+
+export type QueryStudentArgs = {
+  id: Scalars['Int']['input'];
+};
+
+
+export type QueryStudentProgressArgs = {
+  courseId: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+};
+
+
+export type QueryTeacherArgs = {
+  id: Scalars['Int']['input'];
+};
+
+export type Student = {
+  __typename?: 'Student';
+  completedLessons: Array<LessonCompletion>;
+  courses: Array<Course>;
+  email?: Maybe<Scalars['String']['output']>;
+  id: Scalars['Int']['output'];
+  name: Scalars['String']['output'];
+};
+
+export type Teacher = {
+  __typename?: 'Teacher';
+  area?: Maybe<Scalars['String']['output']>;
+  courses: Array<Course>;
+  id: Scalars['Int']['output'];
+  name: Scalars['String']['output'];
+};
+
+export type CompleteLessonMutationVariables = Exact<{
+  input: CompleteLessonInput;
+}>;
+
+
+export type CompleteLessonMutation = { __typename?: 'Mutation', completeLesson: { __typename?: 'LessonCompletion', course_id: number, sequence_number: number, student_id: number, finished: string, lesson: { __typename?: 'Lesson', title: string }, student: { __typename?: 'Student', name: string } } };
+
+export type CreateCourseMutationVariables = Exact<{
+  input: CreateCourseInput;
+}>;
+
+
+export type CreateCourseMutation = { __typename?: 'Mutation', createCourse: { __typename?: 'Course', id: number, title: string, description?: string | null, duration_hours?: number | null, teacher_id: number, teacher: { __typename?: 'Teacher', id: number, name: string } } };
+
+export type CreateLessonMutationVariables = Exact<{
+  input: CreateLessonInput;
+}>;
+
+
+export type CreateLessonMutation = { __typename?: 'Mutation', createLesson: { __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null } };
+
+export type CreateStudentMutationVariables = Exact<{
+  input: CreateStudentInput;
+}>;
+
+
+export type CreateStudentMutation = { __typename?: 'Mutation', createStudent: { __typename?: 'Student', id: number, name: string, email?: string | null } };
+
+export type CreateTeacherMutationVariables = Exact<{
+  input: CreateTeacherInput;
+}>;
+
+
+export type CreateTeacherMutation = { __typename?: 'Mutation', createTeacher: { __typename?: 'Teacher', id: number, name: string, area?: string | null } };
+
+export type DeleteCourseMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+}>;
+
+
+export type DeleteCourseMutation = { __typename?: 'Mutation', deleteCourse: boolean };
+
+export type DeleteLessonMutationVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+}>;
+
+
+export type DeleteLessonMutation = { __typename?: 'Mutation', deleteLesson: boolean };
+
+export type DeleteStudentMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+}>;
+
+
+export type DeleteStudentMutation = { __typename?: 'Mutation', deleteStudent: boolean };
+
+export type DeleteTeacherMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+}>;
+
+
+export type DeleteTeacherMutation = { __typename?: 'Mutation', deleteTeacher: boolean };
+
+export type EnrollStudentMutationVariables = Exact<{
+  input: EnrollStudentInput;
+}>;
+
+
+export type EnrollStudentMutation = { __typename?: 'Mutation', enrollStudent: { __typename?: 'CourseEnrollment', course_id: number, student_id: number, course: { __typename?: 'Course', title: string }, student: { __typename?: 'Student', name: string } } };
+
+export type GetCoursesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetCoursesQuery = { __typename?: 'Query', courses: Array<{ __typename?: 'Course', id: number, title: string, description?: string | null, duration_hours?: number | null, teacher_id: number, teacher: { __typename?: 'Teacher', id: number, name: string, area?: string | null }, lessons: Array<{ __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null }>, students: Array<{ __typename?: 'Student', id: number, name: string, email?: string | null }> }> };
+
+export type GetEnrollmentsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetEnrollmentsQuery = { __typename?: 'Query', enrollments: Array<{ __typename?: 'CourseEnrollment', course_id: number, student_id: number, course: { __typename?: 'Course', id: number, title: string, teacher: { __typename?: 'Teacher', name: string } }, student: { __typename?: 'Student', id: number, name: string, email?: string | null } }> };
+
+export type GetLessonsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetLessonsQuery = { __typename?: 'Query', lessons: Array<{ __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null, course: { __typename?: 'Course', id: number, title: string, teacher: { __typename?: 'Teacher', name: string } } }> };
+
+export type GetLessonsByCourseQueryVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+}>;
+
+
+export type GetLessonsByCourseQuery = { __typename?: 'Query', lessonsByCourse: Array<{ __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null }> };
+
+export type GetStudentsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetStudentsQuery = { __typename?: 'Query', students: Array<{ __typename?: 'Student', id: number, name: string, email?: string | null, courses: Array<{ __typename?: 'Course', id: number, title: string, teacher: { __typename?: 'Teacher', name: string } }>, completedLessons: Array<{ __typename?: 'LessonCompletion', course_id: number, sequence_number: number, student_id: number, finished: string, lesson: { __typename?: 'Lesson', course_id: number, sequence_number: number, title: string } }> }> };
+
+export type GetStudentProgressQueryVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+}>;
+
+
+export type GetStudentProgressQuery = { __typename?: 'Query', studentProgress: Array<{ __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null }> };
+
+export type GetTeachersQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetTeachersQuery = { __typename?: 'Query', teachers: Array<{ __typename?: 'Teacher', id: number, name: string, area?: string | null, courses: Array<{ __typename?: 'Course', id: number, title: string, description?: string | null, duration_hours?: number | null }> }> };
+
+export type UncompleteLessonMutationVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+}>;
+
+
+export type UncompleteLessonMutation = { __typename?: 'Mutation', uncompleteLesson: boolean };
+
+export type UnenrollStudentMutationVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+  studentId: Scalars['Int']['input'];
+}>;
+
+
+export type UnenrollStudentMutation = { __typename?: 'Mutation', unenrollStudent: boolean };
+
+export type UpdateCourseMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+  input: CreateCourseInput;
+}>;
+
+
+export type UpdateCourseMutation = { __typename?: 'Mutation', updateCourse?: { __typename?: 'Course', id: number, title: string, description?: string | null, duration_hours?: number | null, teacher_id: number, teacher: { __typename?: 'Teacher', id: number, name: string } } | null };
+
+export type UpdateLessonMutationVariables = Exact<{
+  courseId: Scalars['Int']['input'];
+  sequenceNumber: Scalars['Int']['input'];
+  title?: InputMaybe<Scalars['String']['input']>;
+  content?: InputMaybe<Scalars['String']['input']>;
+}>;
+
+
+export type UpdateLessonMutation = { __typename?: 'Mutation', updateLesson?: { __typename?: 'Lesson', course_id: number, sequence_number: number, title: string, content?: string | null } | null };
+
+export type UpdateStudentMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+  input: CreateStudentInput;
+}>;
+
+
+export type UpdateStudentMutation = { __typename?: 'Mutation', updateStudent?: { __typename?: 'Student', id: number, name: string, email?: string | null } | null };
+
+export type UpdateTeacherMutationVariables = Exact<{
+  id: Scalars['Int']['input'];
+  input: CreateTeacherInput;
+}>;
+
+
+export type UpdateTeacherMutation = { __typename?: 'Mutation', updateTeacher?: { __typename?: 'Teacher', id: number, name: string, area?: string | null } | null };
